139b3ed0309c000bc4190591996d8dc8f7e0c6ff
Yusuf Uzun <uzefun38@gmail.com>
2023-02-12 14:50:57 +0100

    implenting react

---
+++ frontend/Designer.jsx
@@ -0,0 +1,14 @@
+import "./design.css"
+
+ export default function Design(){
+    return(
+        <>
+            <div className = "ChatScreen">
+                <Input>
+                    <input type="text" name="ChatInput"></input>
+                </Input>
+
+            </div>
+        </>
+    )
+ }
