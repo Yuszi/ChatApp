6e895bd944654ea1fc83e24490569de4bdadb81f
Yusuf Uzun <uzefun38@gmail.com>
2023-02-12 14:59:23 +0100

    ...

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
+
+app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: true}));
 
-app.post("/authenticate", async (req, res) => {
-    const { username } = req.body;
-    return res.json({ username: username, password: "sha256..."})
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
 })
+.then(()=> {
+    console.log("Db Connection Successfull");
+})
+.catch((err) => {
+    console.log(err.message);
+});
 
-app.listen(3001);
+const server = app.listen(process.env.PORT, () => {
+    console.log('Server Started on Port', process.env.PORT);
+});
+
